883cffa7c75fd45298d6dd39b06a638eb0b4b61c
Wojciech Piech <ilikeahottodogu@gmail.com>
2024-02-11 15:15:17 +0100

    date parameter added

--- routes/lastMessage.py
+++ routes/lastMessage.py
@@ -11,6 +11,8 @@ async def last_message():
         lastMessage = soup.find_all('td')
         title = lastMessage[0]
         sender = lastMessage[3]
+        date = lastMessage[4]
+        date = date.text
         idd = lastMessage[6]
         idd = str(idd)
         idd = idd.replace('<td style="text-align:center;"><input name="skasuj_odebrane[]" rel="', '')
@@ -22,10 +24,11 @@ async def last_message():
         messageContent = soup2.find_all('p')
         content = ''
         for messageContentUwU in messageContent:
-            content +=messageContentUwU.text
+            content += messageContentUwU.text
 
     return {
         'title': f'{title.text}',
         'sender': f'{sender.text}',
+        'date': f'{date}',
         'content': f'{content}'
     }
