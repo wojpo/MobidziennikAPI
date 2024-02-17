a00307033409287eda4ff1ff9bffcb24968f70b5
Wojciech Piech <ilikeahottodogu@gmail.com>
2024-02-17 23:39:20 +0100

    small change

--- routes/lastMessage.py
+++ routes/lastMessage.py
@@ -4,9 +4,9 @@ from bs4 import BeautifulSoup
 
 
 async def last_message():
-    with requests.session() as uwu:
-        uwu.post(loginUrl, data=payload)
-        messageSite = uwu.get(messagesUrl)
+    with requests.session() as s:
+        s.post(loginUrl, data=payload)
+        messageSite = s.get(messagesUrl)
         soup = BeautifulSoup(messageSite.content, 'html.parser')
         lastMessage = soup.find_all('td')
         title = lastMessage[0]
@@ -19,7 +19,7 @@ async def last_message():
         idd = idd[:6]
         lastMessageUrlSite = lastMessageUrl
         lastMessageUrlSite += idd
-        LastMessageSite = uwu.get(lastMessageUrlSite)
+        LastMessageSite = s.get(lastMessageUrlSite)
         soup2 = BeautifulSoup(LastMessageSite.content, 'html.parser')
         messageContent = soup2.find_all('p')
         content = ''
